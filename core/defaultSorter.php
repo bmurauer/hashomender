b6727ab952384af8c940b29629c83719c1eb7e44
Benjamin Murauer <benjamin.murauer@student.uibk.ac.at>
2012-12-04 17:05:48 +0100

    added license information

--- core/defaultSorter.php
+++ core/defaultSorter.php
@@ -1,4 +1,21 @@
 <?php
+//  Copyright Benjamin Murauer 2012
+//      
+//  This file is part of the Hash-O-Mender program.
+//
+//  Hash-O-Mender is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Hash-O-Mender is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Hash-O-Mender (gpl.txt).
+//  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Description of defaultSorter
